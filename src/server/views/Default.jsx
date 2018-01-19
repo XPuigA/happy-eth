19588758fb722f20e7159b7afd3a939532ec4476
Xavi Puig <xpuig89@gmail.com>
2018-01-19 21:02:51 +0100

    Fixed libraries

--- src/server/views/Default.jsx
+++ src/server/views/Default.jsx
@@ -4,6 +4,9 @@ export default class Default extends React.Component {
   render() {
     return (
       <html>
+        <head>
+          <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+        </head>
         <body>
           <div id="app" />
           <script src="app.js" />
